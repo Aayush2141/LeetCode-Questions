d6866bb83d71c03207865816609a2d5ab51aa06e
Aayush Singh <aayushbt1201@gmail.com>
2026-07-22 04:25:28 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -0,0 +1,14 @@
+# Last updated: 22/07/2026, 04:25:28
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        
+        for s in strs:
+            key = tuple(sorted(s))
+            
+            if key not in groups:
+                groups[key] = []
+            
+            groups[key].append(s)
+            
+        return list(groups.values())
