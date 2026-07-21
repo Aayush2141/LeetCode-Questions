e085d514e5e9de025e1feb54fa717582f8d917ce
Aayush Singh <aayushbt1201@gmail.com>
2026-07-22 04:24:05 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.py
@@ -0,0 +1,7 @@
+# Last updated: 22/07/2026, 04:24:05
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s) - 1):
+            ans += abs(ord(s[i]) - ord(s[i+1]))
+        return ans
