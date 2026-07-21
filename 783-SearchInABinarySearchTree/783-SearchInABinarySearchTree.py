0d71b94fad7eabaf3c01951ea61f335007e80771
Aayush Singh <aayushbt1201@gmail.com>
2026-07-22 04:24:36 +0530

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.py
@@ -0,0 +1,19 @@
+# Last updated: 22/07/2026, 04:24:36
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def rec(root):
+            if not root:
+                return
+            
+            if root.val==val:
+                return root
+            
+            return rec(root.left) if val<root.val else rec(root.right)
+        
+        return rec(root)
