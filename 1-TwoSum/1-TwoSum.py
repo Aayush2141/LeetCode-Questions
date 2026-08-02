33666429d20ec6c6db27ff2247cd407a071ec1d7
Aayush Singh <aayushbt1201@gmail.com>
2026-08-03 03:43:45 +0530

    File updated at 03/08/2026, 03:43:45

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,20 +1,21 @@
-# Last updated: 22/07/2026, 04:25:42
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic={}
-        arr=[]
-        # for i in nums:
-        #     if i in dic:
-        #         dic[i]+=1
-        #     else:
-        #         dic[i]=1
-        
-        for i in range(len(nums)):
-            val=target-nums[i]
-
-            if val in dic:
-                return [i,dic[val]]
-            else:
-                dic[nums[i]]=i
-
-        return arr
+# Last updated: 03/08/2026, 03:43:45
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        dic={}
+4        arr=[]
+5        # for i in nums:
+6        #     if i in dic:
+7        #         dic[i]+=1
+8        #     else:
+9        #         dic[i]=1
+10        
+11        for i in range(len(nums)):
+12            val=target-nums[i]
+13
+14            if val in dic:
+15                return [i,dic[val]]
+16            else:
+17                dic[nums[i]]=i
+18
+19        return arr
+20
