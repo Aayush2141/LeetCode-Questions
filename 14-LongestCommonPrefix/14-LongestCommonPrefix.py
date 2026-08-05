3787d34573b1cb190c3243e73dd23bba25c99e1b
Aayush Singh <aayushbt1201@gmail.com>
2026-08-06 01:15:33 +0530

    File updated at 06/08/2026, 01:15:32

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,21 +1,21 @@
-# Last updated: 22/07/2026, 04:25:39
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        ss=sorted(strs)
-
-        first=ss[0]
-        last=ss[-1]
-
-        limit=min(len(first),len(last))
-
-        result=""
-        for i in range(limit):
-            if first[i]==last[i]:
-                result+=first[i]
-            else:
-                break
-
-        return result
-
-
-        
+# Last updated: 06/08/2026, 01:15:32
+1class Solution:
+2    def longestCommonPrefix(self, strs: List[str]) -> str:
+3        ss=sorted(strs)
+4
+5        first=ss[0]
+6        last=ss[-1]
+7
+8        limit=min(len(first),len(last))
+9
+10        result=""
+11        for i in range(limit):
+12            if first[i]==last[i]:
+13                result+=first[i]
+14            else:
+15                break
+16
+17        return result
+18
+19
+20        
